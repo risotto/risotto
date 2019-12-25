1ba9967658a65216851a3dbe9ffac6f70248b0d3
rvigee <contact@raphaelvigee.com>
2019-12-25 17:03:44 +0100

    Move TokenType to file

--- src/lib/tokenizer/Token.h
+++ src/lib/tokenizer/Token.h
@@ -9,7 +9,7 @@ extern "C" {
 #include <lib/vm/value.h>
 }
 #include <string>
-#include "../../../lib/better-enums/enum.h"
+#include "TokenType.h"
 
 class Position {
 public:
@@ -21,35 +21,6 @@ public:
     std::string toString();
 };
 
-BETTER_ENUM(TokenType, int,
-// Single-character tokens.
-            LEFT_PAREN, RIGHT_PAREN, LEFT_CURLY, RIGHT_CURLY, LEFT_SQUARED, RIGHT_SQUARED,
-            COMMA, DOT, MINUS, PLUS, PERCENT, SEMICOLON, COLON, SLASH, STAR,
-
-// One or two character tokens.
-            QUESTION_MARK,
-            BANG, BANG_EQUAL,
-            EQUAL, EQUAL_EQUAL,
-            GREATER, GREATER_EQUAL,
-            LESS, LESS_EQUAL,
-            COLON_EQUAL,
-            PLUS_EQUAL,
-            MINUS_EQUAL,
-            SLASH_EQUAL,
-            STAR_EQUAL,
-            PLUS_PLUS,
-            MINUS_MINUS,
-            AMPERSAND,
-
-// Literals.
-            IDENTIFIER, STRING, INT, DOUBLE,
-
-// Keywords
-            AND, ELSE, FALSE, FUNC, FOR, IF, NIL, OR,
-            RETURN, THIS, TRUE, WHILE, NEW, TYPE, STRUCT, OP,
-
-            TOKEN_EOF
-)
 
 class Token {
 public:
